654e9c843fe084f7c9e4d2452453413beaf22720
Amal Narang <56355518+utopian-monkey@users.noreply.github.com>
2022-10-14 05:56:28 +0530

    add sanitiser file

---
+++ sanitiser.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(string username, string password)
+{
+   string arr[]= {"--"," or "," #","/*", ";"," ","'",};
+   username= username+" ";
+   password=password+" ";
+   for(int i=0;i<arr.size();i++)
+   {
+        for(int j=0;j<username.length()-arr[i].length();j++)
+        {
+            if(username.substr(j,arr[i].length())==arr[i])
+            {
+                username.replace(j,arr[i].length(),"");
+            }
+        }
+        for(int j=0;j<password.length()-arr[i].length();j++)
+        {
+            if(password.substr(j,arr[i].length())==arr[i])
+            {
+                password.replace(j,arr[i].length(),"");
+            }
+        }
+   }
+
+   cout<<" The sanitised username is "<<username<<endl;
+   cout<<" The sanitised password is "<<password<<endl;
+}
